55f13406b85a9d56c6c010cf406d68da71c49a6e
Samuel Andrade <samuel.binario@gmail.com>
2023-06-14 08:28:32 -0300

    alteração feita

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ id_do_formulario.addEventListener("submit", function(e){
         const mensagemMaior = `Não é possivel validar o formulario pois o numero ${numeroA} é menor que o numero ${numeroB}`;
         const mensagemMenor = `Formulario validaddo com Sucesso.`;
 
-        if(numeroA > numeroB)
+        if(numeroA < numeroB)
         {
+            const containerMensagemErro = document.querySelector(".mensagem2");
+            containerMensagemErro.innerHTML = mensagemMenor;
+            containerMensagemErro.style.display = 'block';   
+        } else{
             const containerMensagemSucesso = document.querySelector(".mensagem");
             containerMensagemSucesso.innerHTML = mensagemMaior;
             containerMensagemSucesso.style.display = 'block';
-        } else{
-            const containerMensagemErro = document.querySelector(".mensagem2");
-            containerMensagemErro.innerHTML = mensagemMenor;
-            containerMensagemErro.style.display = 'block';
         }
 
     }
